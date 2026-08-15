14fe6e7e90e887d9323fb89d06b623fd529f16ed
Rodrigo Rocha <rodrigorgs@gmail.com>
2026-08-14 21:07:38 -0300

    GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+const [owner, repository] = process.env.GITHUB_REPOSITORY?.split('/') ?? [];
+const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === 'true' && owner && repository;
+const isUserOrOrganizationSite = repository?.endsWith('.github.io');
+
 export default defineConfig({
   output: 'static',
   trailingSlash: 'never',
+  site: isGitHubPagesBuild ? `https://${owner}.github.io` : undefined,
+  base: isGitHubPagesBuild && !isUserOrOrganizationSite ? `/${repository}` : '/',
 });
